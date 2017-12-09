794e533e857d239e8db5def159686b06c5b37b29
Bogdan Kornev <kornev.b@gmail.com>
2017-12-09 13:21:10 +0300

    add swipe refresh layout

--- sample/src/main/java/com/shuhart/bubblepagerindicator/sample/MainActivity.java
+++ sample/src/main/java/com/shuhart/bubblepagerindicator/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.shuhart.bubblepagerindicator.sample;
 
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,10 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager pager = findViewById(R.id.pager);
-        BubblePageIndicator indicator = findViewById(R.id.indicator);
-        ViewPagerAdapter adapter = new ViewPagerAdapter();
+        final ViewPager pager = findViewById(R.id.pager);
+        final BubblePageIndicator indicator = findViewById(R.id.indicator);
+        final ViewPagerAdapter adapter = new ViewPagerAdapter();
         pager.setAdapter(adapter);
         indicator.setViewPager(pager, adapter);
+        SwipeRefreshLayout refreshLayout = findViewById(R.id.swipe_refresh_layout);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                indicator.setViewPager(pager, adapter);
+            }
+        });
     }
 }
